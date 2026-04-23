9a107699605615606467aff4f96d643e583d9f8b
Kripsters <126756673+Kripsters@users.noreply.github.com>
2026-04-23 22:02:43 +0300

    bootstrap change to proxy trust

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*'); // trust all proxies (safe if behind a single LB)
         $middleware->web(SetLocale::class)->append(LogFilamentAccess::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
